bc07af5f14851cf0a36179bb27d2181277b5fdaa
Sofia <emmapohjalainen@gmail.com>
2016-09-19 15:16:05 +0100

    working question carousel

---
+++ public/js/index.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+  $('.question-carousel').slick({
+    accessibility: true,
+    adaptiveHeight: true,
+    arrows: true,
+    draggable: true,
+    fade: true,
+    mobileFirst: true,
+    swipe: true
+  });
+});
